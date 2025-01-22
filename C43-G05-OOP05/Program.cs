ce47186fd4794c897a88328ed952ae6f4722867b
sayed123 <sayedmohameddev@gmai.com>
2025-01-22 15:49:11 +0200

    Add project files.

---
+++ C43-G05-OOP05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C43_G05_OOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
